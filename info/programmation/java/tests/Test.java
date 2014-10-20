abedecc47108a1c2269015c9df39ae029eebd3ff
FredEtRick <fred.malard@wanadoo.fr>
2014-10-20 15:31:27 +0200

    pppQ java sdz révisions avancées

--- info/programmation/java/tests/Test.java
+++ info/programmation/java/tests/Test.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Test
 {
 	public static void main(String[] args)
 	{
-		System.out.println("test");
+		float pi = 3.14f;
+		double PI = 3.141592d;
+		System.out.println(pi + " " + PI);
+		System.out.println("Entres un grand nombre puis un très grand");
+		Scanner S = new Scanner (System.in);
+		pi = S.nextFloat();
+		PI = S.nextDouble();
+		System.out.println(pi + " " + PI);
 	}
 }
