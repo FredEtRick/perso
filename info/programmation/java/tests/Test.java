b5ea957961ec5ca7001f54278e5f1303a8a708a2
FredEtRick <fred.malard@wanadoo.fr>
2014-10-21 23:38:20 +0200

    java sdz pppQ&R P1C2 achevé

--- info/programmation/java/tests/Test.java
+++ info/programmation/java/tests/Test.java
@@ -6,11 +6,18 @@ public class Test
 	{
 		float pi = 3.14f;
 		double PI = 3.141592d;
-		System.out.println(pi + " " + PI);
-		System.out.println("Entres un grand nombre puis un très grand");
-		Scanner S = new Scanner (System.in);
-		pi = S.nextFloat();
-		PI = S.nextDouble();
-		System.out.println(pi + " " + PI);
+		System.out.println(pi + (" " + ' ') + PI);
+		char C = 'b' + 'a';
+		System.out.println(C);
+		String s1, s2;
+		s1 = "a";
+		//s2 = 'b';
+		String s3 = "c";
+		String s4;
+		s4 = new String("d");
+		String s5 = new String("e");
+		System.out.println(s1+s3+s4+s5);
+		System.out.println(4+8+47+2+"e"+2);
+		System.out.println((int)(2/3));
 	}
 }
