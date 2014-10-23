15c792421a03768a5ae9c3ea16205e516150cc8a
FredEtRick <fred.malard@wanadoo.fr>
2014-10-23 15:31:23 +0200

    java pppQ révisé (2 jours de retard)

--- info/programmation/java/tests/Test.java
+++ info/programmation/java/tests/Test.java
@@ -4,20 +4,14 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-		float pi = 3.14f;
-		double PI = 3.141592d;
-		System.out.println(pi + (" " + ' ') + PI);
-		char C = 'b' + 'a';
-		System.out.println(C);
-		String s1, s2;
-		s1 = "a";
-		//s2 = 'b';
-		String s3 = "c";
-		String s4;
-		s4 = new String("d");
-		String s5 = new String("e");
-		System.out.println(s1+s3+s4+s5);
-		System.out.println(4+8+47+2+"e"+2);
-		System.out.println((int)(2/3));
+		float deutiere = 2f / 3f;
+		float precis = 2.000000000000000000001f;
+		double entiers = 2+3;
+		double chars = 'a' + 'b';
+		byte chaar = 'e';
+		long hexa = 0xff48e6948acbL;
+		long binaire = 0b011101101101011011110101110L;
+		System.out.println(deutiere + " " + precis + " " + entiers + " " + chars + " " + chaar + " ");
+		System.out.println(hexa + " " + binaire);
 	}
 }
