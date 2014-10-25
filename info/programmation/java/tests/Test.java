0cb520f04009fd4d485884f4a7a2443a6a91bd2f
FredEtRick <fred.malard@wanadoo.fr>
2014-10-25 17:02:06 +0200

    java sdz pppQ&R P1C4 conditions moitié chapitre

--- info/programmation/java/tests/Test.java
+++ info/programmation/java/tests/Test.java
@@ -1,26 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Test
 {
-	public static void main(String[] args)
+	public static void main (String[] args)
 	{
-		float deutiere = 2f / 3f;
-		float precis = 2.000000000000000000001f;
-		double entiers = 2+3;
-		double chars = 'a' + 'b';
-		byte chaar = 'e';
-		long hexa = 0xff48e6948acbL;
-		System.out.println(deutiere + " " + precis + " " + entiers + " " + chars + " " + chaar + " ");
-		System.out.println(hexa);
-		Scanner entree = new Scanner(System.in);
-		System.out.println("Entrez un entier");
-		int entier = entree.nextInt();
-		System.out.println(entier);
-		System.out.println("Entrez un réel");
-		double reel = entree.nextDouble();
-		System.out.println(reel);
-		System.out.println("Entrez du texte");
-		String texte = entree.nextLine();
-		System.out.println(texte);
+		System.out.println(4>3);
 	}
 }
