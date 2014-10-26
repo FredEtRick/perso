a13f6f2130ba51fcd9409a00fa3ddebdf0825045
FredEtRick <fred.malard@wanadoo.fr>
2014-10-26 21:05:10 +0100

    java sdz pppQ&R P1C4 conditions achevé

--- info/programmation/java/tests/Test.java
+++ info/programmation/java/tests/Test.java
@@ -4,6 +4,44 @@ public class Test
 {
 	public static void main (String[] args)
 	{
-		System.out.println(4>3);
+		System.out.println("combien de pattes avez vous ?");
+		Scanner entree = new Scanner(System.in);
+		int pattes = entree.nextInt();
+		switch (pattes)
+		{
+			case 0 :
+				System.out.print("Vous êtes un ");
+				System.out.println("vers de terre");
+				break;
+			case 1 :
+				System.out.print("Vous êtes un ");
+				System.out.println("unijambiste");
+				break;
+			case 2 :
+				System.out.print("Vous êtes un ");
+				System.out.println("humain");
+				break;
+			case 4 :
+				System.out.print("Vous êtes un ");
+				System.out.println("chien");
+				break;
+			case 8 :
+				System.out.print("Vous êtes une ");
+				System.out.println("pieuvre");
+				break;
+			case 12 :
+				System.out.print("Vous êtes un ");
+				System.out.println("human centiped");
+				break;
+			case 1000 :
+				System.out.print("Vous êtes un ");
+				System.out.println("mille pattes");
+				break;
+			default :
+				if (pattes < 0)
+					System.out.println("Vous êtes un menteur");
+				else
+					System.out.println("Vous êtes un extra-terrestre");
+		}
 	}
 }
