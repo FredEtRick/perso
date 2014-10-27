b4c92b511a0f6b8d900364b05de0ee895e5783cc
FredEtRick <fred.malard@wanadoo.fr>
2014-10-27 14:22:38 +0100

    java sdz P1C5 pppQ&R moitié

--- info/programmation/java/tests/Test.java
+++ info/programmation/java/tests/Test.java
@@ -1,47 +1,20 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Test
 {
-	public static void main (String[] args)
+	public static void main(String[] args)
 	{
-		System.out.println("combien de pattes avez vous ?");
-		Scanner entree = new Scanner(System.in);
-		int pattes = entree.nextInt();
-		switch (pattes)
+		Scanner entree = new Scanner (System.in);
+		String continuer;
+		do
 		{
-			case 0 :
-				System.out.print("Vous êtes un ");
-				System.out.println("vers de terre");
-				break;
-			case 1 :
-				System.out.print("Vous êtes un ");
-				System.out.println("unijambiste");
-				break;
-			case 2 :
-				System.out.print("Vous êtes un ");
-				System.out.println("humain");
-				break;
-			case 4 :
-				System.out.print("Vous êtes un ");
-				System.out.println("chien");
-				break;
-			case 8 :
-				System.out.print("Vous êtes une ");
-				System.out.println("pieuvre");
-				break;
-			case 12 :
-				System.out.print("Vous êtes un ");
-				System.out.println("human centiped");
-				break;
-			case 1000 :
-				System.out.print("Vous êtes un ");
-				System.out.println("mille pattes");
-				break;
-			default :
-				if (pattes < 0)
-					System.out.println("Vous êtes un menteur");
-				else
-					System.out.println("Vous êtes un extra-terrestre");
-		}
+			System.out.print("Quel est ton prénom ? ");
+			System.out.println(entree.nextLine());
+			do
+			{
+				System.out.print("true pour continuer false pour arrêter. ");
+				continuer = entree.nextLine();
+			} while (!(continuer.equals("true")) && !(continuer.equals("false")));
+		} while (Boolean.parseBoolean(continuer));
 	}
 }
