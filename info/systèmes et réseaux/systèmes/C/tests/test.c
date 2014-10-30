71a4c14e907aace213cc4300c43526a401f5aad3
FredEtRick <fred.malard@wanadoo.fr>
2014-10-30 01:48:10 +0100

    C sdz pppQ&R P1C4 variables achevé

--- info/systèmes et réseaux/systèmes/C/tests/test.c
+++ info/systèmes et réseaux/systèmes/C/tests/test.c
@@ -3,13 +3,9 @@
 
 int main (int argc, char *argv[])
 {
-	printf("\t\\tprintf\\n\n");
-	int è = 4;
-	printf("%d\n", è);
-	short nombre = 33000;
-	printf("%d\n", nombre);
-	nombre = -100;
-	printf("%d\n", nombre);
+	unsigned int naturel = 3000000000;
+	int relatif = 2000000000;
+	printf("%d%d", naturel, relatif);
 	
 	return 0;
 }
