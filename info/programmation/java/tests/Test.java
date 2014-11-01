9a2d269f3b8e9b41dbcce7a3fe9cbe0de806056a
FredEtRick <fred.malard@wanadoo.fr>
2014-11-01 16:11:16 +0100

    java sdz pppQ&R P1C8 méthodes moitié

--- info/programmation/java/tests/Test.java
+++ info/programmation/java/tests/Test.java
@@ -4,12 +4,47 @@ public class Test
 {
 	public static void main (String[] args)
 	{
-		int T[][] = new int[4][6];
-		for(int[] i : T)
+		int T[][][] = {{{2, 3}, {1, 2}}, {{1, 1}, {0, 0}}};
+		int A[][][][][] = new int[4][6][2][2][3];
+		for (int[][] sousTab : T)
 		{
-			for(int j : i)
-				System.out.print(j);
+			for (int[] SST : sousTab)
+			{
+				for (int element : SST)
+				{
+					System.out.print(element + " ");
+				}
+				System.out.println();
+			}
 			System.out.println();
 		}
+		for (int[][][][] sousTab : A)
+		{
+			for (int[][][] SST : sousTab)
+			{
+				for (int[][] S3T : SST)
+				{
+					for (int[] S4T : S3T)
+					{
+						for (int element : S4T)
+						{
+							System.out.print(element + " ");
+						}
+						System.out.println();
+					}
+					System.out.println();
+				}
+				System.out.println("=====================================");
+			}
+			System.out.println("=====================================");
+			System.out.println("=====================================");
+			System.out.println("=====================================");
+		}
+		System.out.println("\n\n\n\n" + T.length + " " + A.length);
+		System.out.println(tronque(10.64564847684684d, 5));
+	}
+	public static double tronque(double nombre, int precision)
+	{
+		return (double) ((int) (nombre * Math.pow(10, precision)) / 1) / Math.pow(10.d, precision);
 	}
 }
