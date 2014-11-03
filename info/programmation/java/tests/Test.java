2bd535bc14529823015000ac5e111433243aeffc
FredEtRick <fred.malard@wanadoo.fr>
2014-11-03 17:15:21 +0100

    java sdz pppQ&R PARTIE 1 FINIE

--- info/programmation/java/tests/Test.java
+++ info/programmation/java/tests/Test.java
@@ -4,47 +4,17 @@ public class Test
 {
 	public static void main (String[] args)
 	{
-		int T[][][] = {{{2, 3}, {1, 2}}, {{1, 1}, {0, 0}}};
-		int A[][][][][] = new int[4][6][2][2][3];
-		for (int[][] sousTab : T)
-		{
-			for (int[] SST : sousTab)
-			{
-				for (int element : SST)
-				{
-					System.out.print(element + " ");
-				}
-				System.out.println();
-			}
-			System.out.println();
-		}
-		for (int[][][][] sousTab : A)
-		{
-			for (int[][][] SST : sousTab)
-			{
-				for (int[][] S3T : SST)
-				{
-					for (int[] S4T : S3T)
-					{
-						for (int element : S4T)
-						{
-							System.out.print(element + " ");
-						}
-						System.out.println();
-					}
-					System.out.println();
-				}
-				System.out.println("=====================================");
-			}
-			System.out.println("=====================================");
-			System.out.println("=====================================");
-			System.out.println("=====================================");
-		}
-		System.out.println("\n\n\n\n" + T.length + " " + A.length);
-		System.out.println(tronque(10.64564847684684d, 5));
+		String A[] = {"bla", "bla", "blabla"};
+		Integer B[] = {2, 4, 4};
+		afficherTab(A);
+		afficherTab(B);
 	}
-	public static double tronque(double nombre, int precision)
+	public static void afficherTab (Object[] T)
 	{
-		return (double) ((int) (nombre * Math.pow(10, precision)) / 1) / Math.pow(10.d, precision);
+		for (Object element : T)
+		{
+			System.out.print(element + " ");
+		}
+		System.out.println();
 	}
 }
