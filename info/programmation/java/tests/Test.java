7eda7fcf7501056a5bb6a28a650fb3cc82149fa3
FredEtRick <fred.malard@wanadoo.fr>
2014-11-04 01:00:36 +0100

    java sdz début révisions + inventé TP perso conversion bases sous 10

--- info/programmation/java/tests/Test.java
+++ info/programmation/java/tests/Test.java
@@ -4,17 +4,6 @@ public class Test
 {
 	public static void main (String[] args)
 	{
-		String A[] = {"bla", "bla", "blabla"};
-		Integer B[] = {2, 4, 4};
-		afficherTab(A);
-		afficherTab(B);
-	}
-	public static void afficherTab (Object[] T)
-	{
-		for (Object element : T)
-		{
-			System.out.print(element + " ");
-		}
-		System.out.println();
+		
 	}
 }
