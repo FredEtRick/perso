252d300af69a43538fb89bf35a7b83e6b6b8a545
FredEtRick <fred.malard@wanadoo.fr>
2014-11-05 15:27:21 +0100

    java sdz partie 1 revisions complètes terminées

--- info/programmation/java/tests/Test.java
+++ info/programmation/java/tests/Test.java
@@ -1,9 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Test
 {
 	public static void main (String[] args)
 	{
-		
+		System.out.println("sinus de 10 : " + arrondi(-2.9991, 3));
+	}
+	public static double arrondi (double nombre, int precision)
+	{
+		return ((double) (int) (nombre * Math.pow(10, precision))) / Math.pow(10, precision);
 	}
 }
